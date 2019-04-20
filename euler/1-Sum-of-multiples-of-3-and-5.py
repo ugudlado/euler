95965f4fb4c489465e7d4c75372b51c03008763f
Mahesh <mahesh.g@live.com>
2019-04-20 16:26:09 +0530

    Exercise 2 completed

--- euler/1-Sum-of-multiples-of-3-and-5.py
+++ euler/1-Sum-of-multiples-of-3-and-5.py
@@ -1,13 +1,26 @@
 '''
+Problem statement:
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 
 Reference: https://projecteuler.net/problem=1
+
+Notes:
+Time complexity: O(n)
+Space complexity: O(1)
+
+How to run:
+python3 1-sum-of-multiples-of-3-and-5.py 1000
+
 '''
 import sys
+import time
+
+start_time = time.time()
 
 n= int(sys.argv[1])
+
 i=1
 sum = 0
 
@@ -16,3 +29,5 @@ for i in range(1,n):
         sum += i
 
 print(sum)
+
+print("Execution time in seconds:",(time.time() - start_time))
---
+++ sample.py
@@ -0,0 +1,17 @@
+'''
+Problem statement:
+
+Notes:
+Time complexity: 
+Space complexity: 
+
+How to run? :
+python3 sample.py
+'''
+import time
+
+start_time = time.time()
+
+# Code goes here
+
+print("Execution time in seconds:",time.time() - start_time)
